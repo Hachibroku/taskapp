6ea3ca6aaec4f4f5240e8743ea2013164be86907
Murphey Osmundson <mosmundson88@gmail.com>
2023-06-07 12:27:28 -0400

    Final submission

--- projects/views.py
+++ projects/views.py
@@ -1,7 +1,6 @@
 from django.shortcuts import render, get_object_or_404, redirect
-from projects.models import Project
 from django.contrib.auth.decorators import login_required
-from tasks.models import Task
+from projects.models import Project
 from projects.forms import ProjectForm
 
 
