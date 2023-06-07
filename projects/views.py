35eeb4ad5122a5d630fbf75983b21c4ecab6004d
Murphey Osmundson <mosmundson88@gmail.com>
2023-06-06 21:04:39 -0400

    Features 13, 14, 15, 16 complete - was stuck on 13, worked ahead in the meantime

--- projects/views.py
+++ projects/views.py
@@ -1,6 +1,8 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404, redirect
 from projects.models import Project
 from django.contrib.auth.decorators import login_required
+from tasks.models import Task
+from projects.forms import ProjectForm
 
 
 # Create your views here.
@@ -9,3 +11,22 @@ def list_projects(request):
     projects = Project.objects.filter(owner=request.user)
     context = {"projects": projects}
     return render(request, "projects/list.html", context)
+
+
+@login_required
+def show_project(request, id):
+    project = get_object_or_404(Project, id=id)
+    context = {"project_object": project}
+    return render(request, "projects/detail.html", context)
+
+
+def create_project(request):
+    if request.method == "POST":
+        form = ProjectForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect("list_projects")
+    else:
+        form = ProjectForm()
+    context = {"form": form}
+    return render(request, "projects/create.html", context)
